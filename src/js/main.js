83ca222bcf61cc4120be456f4308cfcf6a91ba03
Siarhei Navitski <sergiej.nowicki@gmail.com>
2022-06-07 11:09:03 +0200

    update code js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,24 @@
 "use strict";
-document.getElementById("btn").addEventListener("click", function () {
+
+const button = document.getElementById("btn");
+const popup = button.addEventListener("click", function () {
 	document.querySelector(".bg-popup").style.display = "flex";
 });
-document.querySelector(".close").addEventListener("click", function () {
-	document.querySelector(".bg-popup").style.display = "none";
-});
+const xButton = document
+	.querySelector(".close")
+	.addEventListener("click", function () {
+		document.querySelector(".bg-popup").style.display = "none";
+	});
+
+let counter = 0;
+
+function elementClick() {
+	counter++;
+	button.innerText = `clicked ${counter}`;
+
+	if (counter >= 5) {
+		button.removeEventListener("click", elementClick);
+	}
+}
+
+button.addEventListener("click", elementClick);
